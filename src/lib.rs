90f7f1a80c1b8df096d90830e31cb5b45e97d4c1
Shohei Wada <pc@wada314.jp>
2024-11-10 11:44:23 +0900

    nyomo

--- src/lib.rs
+++ src/lib.rs
@@ -13,14 +13,19 @@
 // limitations under the License.
 
 #![cfg_attr(feature = "nightly", feature(allocator_api))]
+#![cfg_attr(feature = "nightly", feature(box_into_inner))]
 
-use ::allocator_api2::{Allocator, Global};
+use ::allocator_api2::alloc::{Allocator, Global};
+use ::allocator_api2::boxed::Box;
+use ::std::fmt::Debug;
 
 #[cfg(not(feature = "sync"))]
 use ::std::cell::OnceCell;
 #[cfg(feature = "sync")]
 use ::std::sync::OnceLock as OnceCell;
 
+/// A single linked list which behaves like [`std::cell::OnceCell`], but for multiple values.
+/// See the crate document for the examples.
 #[derive(Clone)]
 pub struct OnceList<T, A: Allocator = Global> {
     head: OnceCell<Box<Cons<T, A>, A>>,
@@ -34,6 +39,7 @@ struct Cons<T, A: Allocator> {
 }
 
 impl<T> OnceList<T, Global> {
+    /// Creates a new empty `OnceList`. This method does not allocate.
     pub fn new() -> Self {
         Self {
             head: OnceCell::new(),
@@ -42,23 +48,27 @@ impl<T> OnceList<T, Global> {
     }
 }
 impl<T, A: Allocator> OnceList<T, A> {
+    /// Creates a new empty `OnceList` with the given allocator. This method does not allocate.
     pub fn new_in(alloc: A) -> Self {
         Self {
             head: OnceCell::new(),
             alloc,
         }
     }
+
+    /// Returns an iterator over the `&T` references in the list.
     pub fn iter(&self) -> impl Iterator<Item = &T> {
-        let mut next = &self.head;
-        ::std::iter::from_fn(move || match next.get() {
+        let mut next_cell = &self.head;
+        ::std::iter::from_fn(move || match next_cell.get() {
             Some(c) => {
-                next = &c.next;
+                next_cell = &c.next;
                 Some(&c.val)
             }
             None => None,
         })
     }
 
+    /// Clears the list, dropping all values.
     pub fn clear(&mut self) {
         self.head = OnceCell::new();
     }
@@ -70,79 +80,87 @@ impl<T, A: Allocator> OnceList<T, A> {
 
 impl<T: Sized, A: Allocator + Clone> OnceList<T, A> {
     pub fn push(&self, val: T) -> &T {
-        let mut next = &self.head;
-        let mut val_cons = Box::new_in(Cons::new(val), self.alloc.clone());
+        let mut next_cell = &self.head;
+        let mut boxed_cons = Box::new_in(Cons::new(val), self.alloc.clone());
         loop {
-            match next.try_insert(val_cons) {
-                Ok(c) => return &c.val,
-                Err((inner, new_val_cons)) => {
-                    next = &inner.next;
-                    val_cons = new_val_cons;
+            match next_cell.set(boxed_cons) {
+                Ok(()) => {
+                    // Safe because we are sure the `next` value is set.
+                    return &unsafe { next_cell.get().unwrap_unchecked() }.val;
+                }
+                Err(new_val_cons) => {
+                    // Safe because we are sure the `next` value is set.
+                    next_cell = &unsafe { next_cell.get().unwrap_unchecked() }.next;
+                    boxed_cons = new_val_cons;
                 }
             }
         }
     }
 
-    pub fn find<P: Fn(&T) -> bool>(&self, pred: P) -> Option<&T> {
-        let mut next = &self.head;
-        while let Some(c) = next.get() {
+    pub fn find<P: FnMut(&T) -> bool>(&self, mut pred: P) -> Option<&T> {
+        let mut next_cell = &self.head;
+        while let Some(c) = next_cell.get() {
             if pred(&c.val) {
                 return Some(&c.val);
             }
-            next = &c.next;
+            next_cell = &c.next;
         }
         None
     }
 
-    pub fn find_map<F: Fn(&T) -> Option<&U>, U>(&self, f: F) -> Option<&U> {
-        let mut next = &self.head;
-        while let Some(c) = next.get() {
+    pub fn find_map<F: FnMut(&T) -> Option<&U>, U>(&self, mut f: F) -> Option<&U> {
+        let mut next_cell = &self.head;
+        while let Some(c) = next_cell.get() {
             if let Some(u) = f(&c.val) {
                 return Some(u);
             }
-            next = &c.next;
+            next_cell = &c.next;
         }
         None
     }
 
-    pub fn remove<P>(&mut self, pred: P) -> Option<T>
+    pub fn remove<P>(&mut self, mut pred: P) -> Option<T>
     where
-        P: Fn(&T) -> bool,
+        P: FnMut(&T) -> bool,
     {
-        let mut next = &mut self.head;
-        while let Some(mut taken_next) = next.take() {
-            if pred(&taken_next.val) {
+        let mut next_cell = &mut self.head;
+        while let Some(next_box) = next_cell.take() {
+            if pred(&next_box.val) {
+                let mut next_val = Box::into_inner(next_box);
+
                 // reconnect the list
-                if let Some(next_next) = taken_next.next.take() {
-                    let _ = next.set(next_next);
+                if let Some(next_next) = next_val.next.take() {
+                    let _ = next_cell.set(next_next);
                 }
-                return Some(taken_next.val);
+                return Some(next_val.val);
             }
 
-            let _ = next.set(taken_next);
-            next = &mut unsafe { next.get_mut().unwrap_unchecked() }.next;
+            let _ = next_cell.set(next_box);
+            // Safe because we are sure the `next_cell` value is set.
+            next_cell = &mut unsafe { next_cell.get_mut().unwrap_unchecked() }.next;
         }
         None
     }
 
-    pub fn remove_map<F, U>(&mut self, f: F) -> Option<U>
+    pub fn remove_map<F, U>(&mut self, mut f: F) -> Option<U>
     where
-        F: Fn(T) -> Result<U, T>,
+        F: FnMut(T) -> Result<U, T>,
     {
-        let mut next = &mut self.head;
-        while let Some(mut taken_next) = next.take() {
-            match f(taken_next.val) {
+        let mut next_cell = &mut self.head;
+        while let Some(next_box) = next_cell.take() {
+            let next_val = Box::into_inner(next_box);
+            match f(next_val.val) {
                 Ok(u) => {
                     // reconnect the list
-                    if let Some(next_next) = taken_next.next.take() {
-                        let _ = next.set(next_next);
+                    if let Some(next_next) = next_val.next.take() {
+                        let _ = next_cell.set(next_next);
                     }
                     return Some(u);
                 }
                 Err(t) => {
-                    taken_next.val = t;
-                    let _ = next.set(taken_next);
-                    next = &mut unsafe { next.get_mut().unwrap_unchecked() }.next;
+                    next_val.val = t;
+                    let _ = next_cell.set(next_box);
+                    next_cell = &mut unsafe { next_cell.get_mut().unwrap_unchecked() }.next;
                 }
             }
         }
@@ -156,7 +174,7 @@ impl<T> Default for OnceList<T, Global> {
     }
 }
 
-impl<T: Debug, A> Debug for OnceList<T, A> {
+impl<T: Debug, A: Allocator> Debug for OnceList<T, A> {
     fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
         f.debug_list().entries(self.iter()).finish()
     }

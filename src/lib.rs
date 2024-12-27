0520411a9f0333df24659cefca1523d5e882e133
wada314 <pc@wada314.jp>
2024-12-27 19:45:51 +0900

    Add unsized support (#1)
    * adding ?Sized
    
    * edit tasks.json
    
    * woofoo
    
    * foobar
    
    * zundoko
    
    * muzusugi!
    
    * hoorayyy
    
    * tested
    
    * still needs code refactoriong
    
    * foobar
    
    * yeah
    
    * a
    
    * testing
    
    * redun
    
    * sort impls
    
    * documenting
    
    * nightly warning in doc
    
    * nit

--- src/lib.rs
+++ src/lib.rs
@@ -15,10 +15,19 @@
 #![doc = include_str!("../README.md")]
 #![cfg_attr(feature = "nightly", feature(allocator_api))]
 #![cfg_attr(feature = "nightly", feature(box_into_inner))]
+#![cfg_attr(feature = "nightly", feature(coerce_unsized))]
+#![cfg_attr(feature = "nightly", feature(doc_cfg))]
+#![cfg_attr(feature = "nightly", feature(once_cell_try_insert))]
+#![cfg_attr(feature = "nightly", feature(ptr_metadata))]
+#![cfg_attr(feature = "nightly", feature(unsize))]
 
+#[cfg(feature = "nightly")]
+use ::allocator_api2::alloc;
 use ::allocator_api2::alloc::{Allocator, Global};
 use ::allocator_api2::boxed::Box;
 use ::std::fmt::Debug;
+#[cfg(feature = "nightly")]
+use ::std::marker::Unsize;
 use ::std::ops::DerefMut;
 
 #[cfg(not(feature = "sync"))]
@@ -27,20 +36,97 @@ use ::std::cell::OnceCell;
 use ::std::sync::OnceLock as OnceCell;
 
 /// A single linked list which behaves like [`std::cell::OnceCell`], but for multiple values.
-/// See the crate document for the examples.
+///
+/// # Usage
+///
+/// A simple example:
+///
+/// ```rust
+/// use once_list2::OnceList;
+///
+/// // Create a new empty list. Note that the variable is immutable.
+/// let list = OnceList::<i32>::new();
+///
+/// // You can push values to the list without the need for mutability.
+/// list.push(1);
+/// list.push(2);
+///
+/// // Or you can push multiple values at once.
+/// list.extend([3, 4, 5]);
+///
+/// // You can iterate over the list.
+/// assert_eq!(list.iter().copied().collect::<Vec<_>>(), vec![1, 2, 3, 4, 5]);
+///
+/// // Some methods are mutable only.
+/// let mut list_mut = list;
+///
+/// // You can remove (take) a value from the list.
+/// let removed = list_mut.remove(|&x| x % 2 == 0);
+/// assert_eq!(removed, Some(2));
+/// assert_eq!(list_mut.iter().copied().collect::<Vec<_>>(), vec![1, 3, 4, 5]);
+/// ```
+///
+/// # (Almost nightly rustc only) Unsized types support
+///
+/// You can use the [unsized types] like `str`, `[u8]` or `dyn Display` as the value type of the `OnceList`.
+///
+/// Though you can use this feature without the nightly rustc compiler, you can not push or extend the values to the list without the nightly compiler and the `nightly` feature enabled. i.e. You can only create an empty list, that's all.
+///
+/// In the nightly compiler and with the `nightly` feature enabled, the additional methods like `push_unsized`, `remove_unsized_as` become available:
+///
+/// ```rust
+/// # #[cfg(not(feature = "nightly"))]
+/// # fn main() {}
+/// # #[cfg(feature = "nightly")]
+/// # fn main() {
+/// // This code only works with the nightly compiler and the `nightly` feature enabled.
+///
+/// use once_list2::OnceList;
+///
+/// // Creating a `OnceList` for `[i32]`, the unsized type.
+/// let list = OnceList::<[i32]>::new();
+///
+/// list.push_unsized([1] /* A sized array type, `[i32; 1]`, can be coerced into [i32].*/);
+/// list.push_unsized([2, 3] /* Same for `[i32; 2] type. */);
+///
+/// // The normal methods like `iter` are available because it returns a reference to the value.
+/// assert_eq!(list.iter().nth(0).unwrap(), &[1]);
+/// assert_eq!(list.iter().nth(1).unwrap(), &[2, 3]);
+///
+/// let mut list_mut = list;
+///
+/// // `remove_unsized_as` method allows you to check the unsized value type and remove it.
+/// let removed: Option<[i32; 2]> = unsafe {
+///     list_mut.remove_unsized_as(|x| if x.len() == 2 {
+///         Some(x.try_into().unwrap())
+///     } else {
+///         None
+///     })
+/// };
+/// // The removed value is an array, not a slice!
+/// assert_eq!(removed, Some([2, 3]));
+/// # }
+/// ```
+/// [unsized types]: https://doc.rust-lang.org/book/ch19-04-advanced-types.html#dynamically-sized-types-and-the-sized-trait
+///
 #[derive(Clone)]
-pub struct OnceList<T, A: Allocator = Global> {
-    head: OnceCell<Box<Cons<T, A>, A>>,
+pub struct OnceList<T: ?Sized, A: Allocator = Global> {
+    head: OnceCell<Box<Cons<T, T, A>, A>>,
     alloc: A,
 }
 
+/// A single linked list node.
+///
+/// Type parameter `T` and `U` are essentially the same type, but for rust's unsized coercion
+/// feature, we need to separate them.
+/// Then we can safely cast `&Cons<SizedT, U, A>` into `&Cons<UnsizedT, U, A>`.
 #[derive(Clone)]
-struct Cons<T, A: Allocator> {
-    next: OnceCell<Box<Cons<T, A>, A>>,
+struct Cons<T: ?Sized, U: ?Sized, A: Allocator> {
+    next: OnceCell<Box<Cons<U, U, A>, A>>,
     val: T,
 }
 
-impl<T> OnceList<T, Global> {
+impl<T: ?Sized> OnceList<T, Global> {
     /// Creates a new empty `OnceList`. This method does not allocate.
     pub fn new() -> Self {
         Self {
@@ -49,7 +135,7 @@ impl<T> OnceList<T, Global> {
         }
     }
 }
-impl<T, A: Allocator> OnceList<T, A> {
+impl<T: ?Sized, A: Allocator> OnceList<T, A> {
     /// Creates a new empty `OnceList` with the given allocator. This method does not allocate.
     pub fn new_in(alloc: A) -> Self {
         Self {
@@ -111,7 +197,7 @@ impl<T, A: Allocator> OnceList<T, A> {
         last_opt
     }
 
-    fn last_cell(&self) -> &OnceCell<Box<Cons<T, A>, A>> {
+    fn last_cell(&self) -> &OnceCell<Box<Cons<T, T, A>, A>> {
         let mut next_cell = &self.head;
         while let Some(next_box) = next_cell.get() {
             next_cell = &next_box.next;
@@ -133,10 +219,10 @@ impl<T, A: Allocator> OnceList<T, A> {
 
     /// Returns an iterator over the `&mut T` references in the list.
     pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut T> {
-        struct Iter<'a, T, A: Allocator> {
-            next_cell: &'a mut OnceCell<Box<Cons<T, A>, A>>,
+        struct Iter<'a, T: ?Sized, A: Allocator> {
+            next_cell: &'a mut OnceCell<Box<Cons<T, T, A>, A>>,
         }
-        impl<'a, T, A: Allocator> Iterator for Iter<'a, T, A> {
+        impl<'a, T: ?Sized, A: Allocator> Iterator for Iter<'a, T, A> {
             type Item = &'a mut T;
             fn next(&mut self) -> Option<Self::Item> {
                 let next_box = self.next_cell.get_mut()?;
@@ -160,26 +246,91 @@ impl<T, A: Allocator> OnceList<T, A> {
     pub fn allocator(&self) -> &A {
         &self.alloc
     }
+}
 
-    /// Find a first value in the list matches the predicate, remove that item from the list,
-    /// and then returns that value.
-    pub fn remove<P>(&mut self, mut pred: P) -> Option<T>
+impl<T: ?Sized, A: Allocator> OnceList<T, A> {
+    /// Removes the first value in the list that matches the predicate, and returns the value as a boxed value.
+    ///
+    /// This method supports the unsized value type `T` as well.
+    ///
+    /// Note that even though this method returns a boxed value, the box is something re-allcoated.
+    /// So this method might not be efficient as you expect.
+    #[cfg(feature = "nightly")]
+    #[cfg_attr(feature = "nightly", doc(cfg(feature = "nightly")))]
+    pub fn remove_into_box<P>(&mut self, pred: P) -> Option<Box<T, A>>
     where
         P: FnMut(&T) -> bool,
+    {
+        self.remove_inner(pred, |boxed_cons| boxed_cons.box_into_inner_box())
+    }
+
+    /// Removes the first value in the list that matches the predicate, and returns the value.
+    ///
+    /// The predicate function `pred` should return `Some(&U)` if the value is found,
+    /// and the returned reference `&U` must be the same address as the value given in the `pred`.
+    ///
+    /// # Safety
+    /// This method is unsafe because it requires the predicate to return a reference to the same address as the value.
+    #[cfg(feature = "nightly")]
+    #[cfg_attr(feature = "nightly", doc(cfg(feature = "nightly")))]
+    pub unsafe fn remove_unsized_as<U, P>(&mut self, mut pred: P) -> Option<U>
+    where
+        P: FnMut(&T) -> Option<&U>,
+    {
+        let found_sized_ptr: OnceCell<*const U> = OnceCell::new();
+        self.remove_inner(
+            |val| {
+                if let Some(val) = pred(val) {
+                    // We only set the value once, so this is safe.
+                    found_sized_ptr.set(val as *const U).unwrap();
+                    true
+                } else {
+                    false
+                }
+            },
+            |boxed_cons| -> U {
+                // Given the boxed cons with the unsized value type `T`,
+                // and returns the sized type value `U` by value (i.e. out of the box).
+
+                // We are sure the `found_sized_ptr` is set.
+                let found_sized_ptr: *const U = *found_sized_ptr.get().unwrap();
+
+                let cons_layout = alloc::Layout::for_value::<Cons<T, T, A>>(&boxed_cons);
+                let (cons_ptr, alloc) = Box::into_non_null_with_allocator(boxed_cons);
+                let val_ptr = &unsafe { cons_ptr.as_ref() }.val as *const T;
+
+                // Double check the ptr returned by the `pred` is the same as the pointer we extracted from the cons.
+                debug_assert_eq!(val_ptr as *const U, found_sized_ptr);
+
+                // Load (memcpy) the value into the output variable.
+                let result = unsafe { ::std::ptr::read(val_ptr as *const U) };
+
+                // Free the cons memory.
+                unsafe { alloc.deallocate(cons_ptr.cast(), cons_layout) };
+
+                result
+            },
+        )
+    }
+
+    /// An inner implementeation for `remove_xxx` methods.
+    fn remove_inner<P, F, U>(&mut self, mut pred: P, mut f: F) -> Option<U>
+    where
+        P: FnMut(&T) -> bool,
+        F: FnMut(Box<Cons<T, T, A>, A>) -> U,
     {
         let mut next_cell = &mut self.head;
         while let Some(next_ref) = next_cell.get() {
             if pred(&next_ref.val) {
                 // Safe because we are sure the `next_cell` value is set.
-                let next_box = next_cell.take().unwrap();
-                let mut next_cons = Box::into_inner(next_box);
+                let mut next_box = next_cell.take().unwrap();
 
                 // reconnect the list
-                if let Some(next_next) = next_cons.next.take() {
+                if let Some(next_next) = next_box.next.take() {
                     let _ = next_cell.set(next_next);
                 }
 
-                return Some(next_cons.val);
+                return Some(f(next_box));
             }
             // Safe because we are sure the `next_cell` value is set.
             next_cell = &mut next_cell.get_mut().unwrap().next;
@@ -188,26 +339,55 @@ impl<T, A: Allocator> OnceList<T, A> {
     }
 }
 
-impl<T: Sized, A: Allocator + Clone> OnceList<T, A> {
+impl<T: ?Sized, A: Allocator + Clone> OnceList<T, A> {
+    /// An unsized version of the [`OnceList::push`] method.
+    ///
+    /// You can push a sized value to the list. For exaple, you can push `[i32; 3]` to the list of `[i32]`.
+    #[cfg(feature = "nightly")]
+    #[cfg_attr(feature = "nightly", doc(cfg(feature = "nightly")))]
+    pub fn push_unsized<U: Unsize<T>>(&self, val: U) -> &U {
+        let boxed_cons = Cons::new_boxed(val, self.alloc.clone());
+        self.push_inner(boxed_cons, |c| unsafe { &*(c as *const T as *const U) })
+    }
+
+    /// An inner implementation for the `push_xxx` methods.
+    fn push_inner<F, U>(&self, mut new_cons: Box<Cons<T, T, A>, A>, f: F) -> &U
+    where
+        F: FnOnce(&T) -> &U,
+    {
+        let mut next_cell = &self.head;
+        loop {
+            match next_cell.try_insert2(new_cons) {
+                Ok(new_cons) => {
+                    return f(&new_cons.val);
+                }
+                Err((cur_cons, new_cons2)) => {
+                    next_cell = &cur_cons.next;
+                    new_cons = new_cons2;
+                }
+            }
+        }
+    }
+}
+
+impl<T, A: Allocator> OnceList<T, A> {
+    /// Find a first value in the list matches the predicate, remove that item from the list,
+    /// and then returns that value.
+    pub fn remove<P>(&mut self, mut pred: P) -> Option<T>
+    where
+        P: FnMut(&T) -> bool,
+    {
+        self.remove_inner(&mut pred, |boxed_cons| Box::into_inner(boxed_cons).val)
+    }
+}
+
+impl<T, A: Allocator + Clone> OnceList<T, A> {
     /// Appends a value to the list, and returns the reference to that value.
     ///
     /// Note that this method takes `&self`, not `&mut self`.
     pub fn push(&self, val: T) -> &T {
-        let mut next_cell = &self.head;
-        let mut boxed_cons = Box::new_in(Cons::new(val), self.alloc.clone());
-        loop {
-            match next_cell.set(boxed_cons) {
-                Ok(()) => {
-                    // Safe because we are sure the `next` value is set.
-                    return &unsafe { next_cell.get().unwrap_unchecked() }.val;
-                }
-                Err(new_val_cons) => {
-                    // Safe because we are sure the `next` value is set.
-                    next_cell = &unsafe { next_cell.get().unwrap_unchecked() }.next;
-                    boxed_cons = new_val_cons;
-                }
-            }
-        }
+        let boxed_cons = Box::new_in(Cons::new(val), self.alloc.clone());
+        self.push_inner(boxed_cons, |c| c)
     }
 
     /// An almost same method with the [`std::iter::Extend::extend`],
@@ -224,13 +404,13 @@ impl<T: Sized, A: Allocator + Clone> OnceList<T, A> {
     }
 }
 
-impl<T> Default for OnceList<T, Global> {
+impl<T: ?Sized> Default for OnceList<T, Global> {
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl<T: Debug, A: Allocator> Debug for OnceList<T, A> {
+impl<T: ?Sized + Debug, A: Allocator> Debug for OnceList<T, A> {
     fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
         f.debug_list().entries(self.iter()).finish()
     }
@@ -256,7 +436,7 @@ impl<T, A: Allocator> IntoIterator for OnceList<T, A> {
     }
 }
 
-pub struct IntoIter<T, A: Allocator>(OnceCell<Box<Cons<T, A>, A>>);
+pub struct IntoIter<T, A: Allocator>(OnceCell<Box<Cons<T, T, A>, A>>);
 
 impl<T, A: Allocator> Iterator for IntoIter<T, A> {
     type Item = T;
@@ -276,12 +456,84 @@ impl<T, A: Allocator + Clone> Extend<T> for OnceList<T, A> {
     }
 }
 
-impl<T, A: Allocator> Cons<T, A> {
+impl<T, U: ?Sized, A: Allocator> Cons<T, U, A> {
     fn new(val: T) -> Self {
         Self {
             next: OnceCell::new(),
             val,
         }
+    }
+}
+
+#[cfg(feature = "nightly")]
+impl<T: ?Sized, A: Allocator> Cons<T, T, A> {
+    fn new_boxed<U>(val: U, alloc: A) -> Box<Self, A>
+    where
+        U: Unsize<T>,
+    {
+        // As mentioned in the [`Cons`]'s document, this unsized coercion cast is safe!
+        Box::<Cons<U, T, A>, A>::new_in(
+            Cons::<U, T, A> {
+                next: OnceCell::new(),
+                val: val,
+            },
+            alloc,
+        )
+    }
+
+    fn box_into_inner_box(self: Box<Self, A>) -> Box<T, A> {
+        use ::std::ptr::{metadata, NonNull};
+
+        let cons_layout = alloc::Layout::for_value::<Cons<T, T, A>>(&self);
+        let layout = alloc::Layout::for_value::<T>(&self.val);
+        let metadata = metadata(&self.val);
+        let (raw_cons, alloc) = Box::into_raw_with_allocator(self);
+        let dst = alloc.allocate(layout).unwrap();
+
+        // Make sure to drop the `cons`'s unused fields.
+        let Cons { next, val } = unsafe { &*raw_cons };
+        let _ = unsafe { ::std::ptr::read(next) };
+        let raw_src = val as *const T;
+
+        // Do memcpy.
+        unsafe {
+            ::std::ptr::copy_nonoverlapping(
+                raw_src.cast::<u8>(),
+                dst.cast::<u8>().as_ptr(),
+                layout.size(),
+            );
+        }
+
+        // free the `cons`'s memory. Not `drop` because we already dropped the fields.
+        unsafe {
+            alloc.deallocate(NonNull::new(raw_cons).unwrap().cast(), cons_layout);
+        }
+
+        // Create a new fat pointer for dst by combining the thin pointer and the metadata.
+        let dst = NonNull::<T>::from_raw_parts(dst.cast::<u8>(), metadata);
+
+        unsafe { Box::from_non_null_in(dst, alloc) }
+    }
+}
+
+/// A workaround for the missing `OnceCell::try_insert` method.
+trait OnceCellExt<T> {
+    fn try_insert2(&self, value: T) -> Result<&T, (&T, T)>;
+}
+#[cfg(not(feature = "nightly"))]
+impl<T> OnceCellExt<T> for OnceCell<T> {
+    fn try_insert2(&self, value: T) -> Result<&T, (&T, T)> {
+        // The both unsafe blocks are safe because it's sure the cell value is set.
+        match self.set(value) {
+            Ok(()) => Ok(unsafe { self.get().unwrap_unchecked() }),
+            Err(value) => Err((unsafe { self.get().unwrap_unchecked() }, value)),
+        }
+    }
+}
+#[cfg(feature = "nightly")]
+impl<T> OnceCellExt<T> for OnceCell<T> {
+    fn try_insert2(&self, value: T) -> Result<&T, (&T, T)> {
+        self.try_insert(value)
     }
 }
 
@@ -385,5 +637,91 @@ mod tests {
 
         assert_eq!(list.remove(|&v| v == 3), Some(3));
         assert!(list.is_empty());
+    }
+
+    #[test]
+    #[cfg(feature = "nightly")]
+    fn test_unsized_slice_push() {
+        let list: OnceList<[i32]> = OnceList::new();
+        let first = list.push_unsized([1]);
+        let second = list.push_unsized([2, 3]);
+        assert_eq!(first, &[1]);
+        assert_eq!(second, &[2, 3]);
+
+        assert_eq!(list.iter().nth(0), Some(&[1] as &[i32]));
+        assert_eq!(list.iter().nth(1), Some(&[2, 3] as &[i32]));
+    }
+
+    #[test]
+    #[cfg(feature = "nightly")]
+    fn test_unsized_dyn_push() {
+        let list: OnceList<dyn ToString> = OnceList::new();
+        let first = list.push_unsized(1);
+        let second = list.push_unsized("hello");
+        assert_eq!(first.to_string(), "1");
+        assert_eq!(second.to_string(), "hello");
+
+        assert_eq!(
+            list.iter().nth(0).map(<dyn ToString>::to_string),
+            Some("1".to_string())
+        );
+        assert_eq!(
+            list.iter().nth(1).map(<dyn ToString>::to_string),
+            Some("hello".to_string())
+        );
+    }
+
+    #[test]
+    #[cfg(feature = "nightly")]
+    fn test_unsized_slice_remove_into_box() {
+        let list = OnceList::<[i32]>::new();
+        list.push_unsized([1]);
+        list.push_unsized([2, 3]);
+        list.push_unsized([4, 5, 6]);
+
+        let mut list = list;
+        let removed = list.remove_into_box(|s| s.len() == 2);
+        assert_eq!(removed, Some(Box::new([2, 3]) as Box<[i32]>));
+        assert_eq!(list.len(), 2);
+        assert_eq!(list.iter().nth(0), Some(&[1] as &[i32]));
+        assert_eq!(list.iter().nth(1), Some(&[4, 5, 6] as &[i32]));
+    }
+
+    #[test]
+    #[cfg(feature = "nightly")]
+    fn test_unsized_dyn_remove_into_box() {
+        let list = OnceList::<dyn ToString>::new();
+        list.push_unsized(1);
+        list.push_unsized("hello");
+        list.push_unsized(42);
+
+        let mut list = list;
+        let removed = list.remove_into_box(|s| s.to_string() == "hello");
+        assert_eq!(removed.map(|s| s.to_string()), Some("hello".to_string()));
+        assert_eq!(list.len(), 2);
+        assert_eq!(
+            list.iter().nth(0).map(|s| s.to_string()),
+            Some("1".to_string())
+        );
+        assert_eq!(
+            list.iter().nth(1).map(|s| s.to_string()),
+            Some("42".to_string())
+        );
+    }
+
+    #[test]
+    #[cfg(feature = "nightly")]
+    fn test_unsized_slice_remove_as() {
+        let list = OnceList::<[i32]>::new();
+        list.push_unsized([1]);
+        list.push_unsized([2, 3]);
+        list.push_unsized([4, 5, 6]);
+
+        let mut list = list;
+        let removed: Option<[i32; 2]> = unsafe { list.remove_unsized_as(|s| s.try_into().ok()) };
+        assert_eq!(removed, Some([2, 3]));
+        assert_eq!(list.len(), 2);
+        assert_eq!(list.iter().nth(0), Some(&[1] as &[i32]));
+        assert_eq!(list.iter().nth(1), Some(&[4, 5, 6] as &[i32]));
     }
 }
